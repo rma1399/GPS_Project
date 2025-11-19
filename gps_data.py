55ea7d6f31d0fce3d835710e09f9fd43f530a3f4
rma1399 <rma1399@rit.edu>
2025-11-19 13:21:24 -0500

    data cleaning setup

--- gps_data.py
+++ gps_data.py
@@ -10,30 +10,29 @@ import pathlib
 def clean_data(folder_filename):
 
     path = pathlib.Path(folder_filename)
-    cols_placeholder1 = range(15)
-    cols_placeholder2 = range(13)
-    data1 = pd.DataFrame()
-    data1.columns = cols_placeholder1
-    data2 = pd.DataFrame()
-    data2.columns = cols_placeholder2
-
-    for file in path.glob("*.txt"):
-        with open(file, 'r') as f:
+    cols_placeholder1 = range(13) #change these to whatever we choose to call them
+    cols_placeholder2 = range(15)
+   
+    GPMRC = pd.DataFrame(columns=cols_placeholder1)
+    GPGGA = pd.DataFrame(columns=cols_placeholder2)
+    
+    for file in path.glob("*.txt"): #grabs all text files in our directory we pass in
+        with open(file, 'r') as f: #there is mixed encoding, so we can not nicely read it into a csv by just skipping lines
             content = f.read()
-            GPMRC = []
-            GPGGA = []
-            for line in content.split('$'):
+            GPMRC_file = []
+            GPGGA_file = []
+            for line in content.split('$'): #not new line because some data is doubled
                 values = line.split(',')
-                if values[0] == 'GPRMC':
-                    GPMRC.append(values)
-                elif values[0] == 'GPGGA':
-                    GPGGA.append(values)
+                if values[0] == 'GPRMC' and len(values) == 13: #majority expected length for MRC
+                    GPMRC_file.append(values)
+                elif values[0] == 'GPGGA' and len(values) == 15: #majority expected length for GGA
+                    GPGGA_file.append(values)
             
-            data1 = pd.merge(left=data1, right=pd.DataFrame(GPMRC, columns=cols_placeholder1))
-            data2 = pd.merge(left=data2, right=pd.DataFrame(GPGGA, columns=cols_placeholder2))
+            GPMRC = pd.concat([GPMRC, pd.DataFrame(GPMRC_file, columns=cols_placeholder1)], ignore_index=True)
+            GPGGA = pd.concat([GPGGA, pd.DataFrame(GPGGA_file, columns=cols_placeholder2)], ignore_index=True)
 
-
-
+    GPMRC.to_csv('GPMRC_GPS_data.csv', index=False)
+    GPGGA.to_csv('GPGGA_GPS_data.csv', index=False)
     return 0
 
 
